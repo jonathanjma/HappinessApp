4b3bbdcceae3ff39e18c9f482202be068daab4b1
Jonathan Ma <jonathanjma03@gmail.com>
2023-01-11 23:16:28 -0800

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
 import "./index.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
 import Statistics from "./pages/Statistics";
 import Profile from "./pages/Profile";
+import Home from "./pages/Home";
+import Header from "./components/Header";
+import Container from "react-bootstrap/Container";
 
 // change id number to index of user in Users.js (temporary until backend + login set up)
 function App() {
   const id = 1;
   return (
-    <BrowserRouter>
-      <div className="bg-rhythm-200">
-        <div className="max-w-7xl mx-auto min-h-screen px-3 py-2">
-          <Routes>
-            <Route path="/statistics" element={<Statistics id={id} />} />
-            <Route path="/profile" element={<Profile id={id} />} />
-          </Routes>
-        </div>
-      </div>
-    </BrowserRouter>
+    <Container fluid className="App bg-rhythm-200">
+      <BrowserRouter>
+        <Header user_id={id} />
+        {/*<div className="max-w-7xl mx-auto min-h-screen px-3 py-2">*/}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/statistics" element={<Statistics id={id} />} />
+          <Route path="/profile" element={<Profile id={id} />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+        {/*</div>*/}
+      </BrowserRouter>
+    </Container>
   );
 }
 
