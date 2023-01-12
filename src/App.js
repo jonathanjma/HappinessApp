7b3114f949899fbe70fb76c9f14fc37387f705a2
Alex967CW <awcyclist3@gmail.com>
2023-01-12 02:43:33 -0500

    package updates

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./index.css";
-import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Statistics from "./pages/Statistics";
 import Profile from "./pages/Profile";
 import Home from "./pages/Home";
@@ -8,19 +8,19 @@ import Container from "react-bootstrap/Container";
 
 // change id number to index of user in Users.js (temporary until backend + login set up)
 function App() {
-  const id = 1;
+  const id = 20;
   return (
     <Container fluid className="App bg-rhythm-200">
       <BrowserRouter>
         <Header user_id={id} />
-        {/*<div className="max-w-7xl mx-auto min-h-screen px-3 py-2">*/}
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/statistics" element={<Statistics id={id} />} />
-          <Route path="/profile" element={<Profile id={id} />} />
-          <Route path="*" element={<Navigate to="/" />} />
-        </Routes>
-        {/*</div>*/}
+        <div className="max-w-7xl mx-auto min-h-screen px-3 py-2">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/statistics" element={<Statistics id={id} />} />
+            <Route path="/profile" element={<Profile id={id} />} />
+            <Route path="*" element={<Navigate to="/" />} />
+          </Routes>
+        </div>
       </BrowserRouter>
     </Container>
   );
