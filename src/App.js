87cc43a674ecd4f23d8440ef2f9d8a0b461bfe89
Jonathan Ma <jonathanjma03@gmail.com>
2023-01-13 00:15:22 -0800

    only show welcome if not "logged in"

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import Statistics from "./pages/Statistics";
 import Profile from "./pages/Profile";
 import Header from "./components/Header";
 import Container from "react-bootstrap/Container";
-import Welcome from "./pages/Welcome";
-import SubmitHappiness from "./pages/SubmitHappiness";
+import Home from "./pages/Home";
 
 // change id number to id of user in Users.js (temporary until backend + login set up)
 export default function App() {
   const id = 20;
+  const logged_in = false;
 
   return (
     <Container fluid className="App bg-rhythm-200">
@@ -16,8 +16,7 @@ export default function App() {
         <Header user_id={id} />
         <div className="max-w-7xl mx-auto min-h-screen px-3 py-2">
           <Routes>
-            <Route path="/" element={<Welcome />} />
-            <Route path="/happiness" element={<SubmitHappiness />} />
+            <Route path="/" element={<Home logged_in={logged_in} />} />
             <Route path="/statistics" element={<Statistics id={id} />} />
             <Route path="/profile" element={<Profile id={id} />} />
             <Route path="*" element={<Navigate to="/" />} />
