6a8a93f600581ff842d58d212b7471672106a866
Fiddle01 <58796478+Fiddle01@users.noreply.github.com>
2023-01-13 21:11:19 -0500

    Welcome page is now on its own, separate from app. isLoggedIn now changes between the welcome page and the app.

--- src/App.js
+++ src/App.js
@@ -2,27 +2,36 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Statistics from "./pages/Statistics";
 import Profile from "./pages/Profile";
 import Header from "./components/Header";
+import Welcome from "./pages/Welcome";
 import Container from "react-bootstrap/Container";
 import Home from "./pages/Home";
+import { useState } from "react";
 
 // change id number to id of user in Users.js (temporary until backend + login set up)
 export default function App() {
   const id = 20;
-  const logged_in = false;
+  //TODO this is a placeholder with basic functionality, we will have to rewrite after backend implementation.
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   return (
-    <Container fluid className="App bg-rhythm-200">
-      <BrowserRouter>
-        <Header user_id={id} />
-        <div className="max-w-7xl mx-auto min-h-screen px-3 py-2">
-          <Routes>
-            <Route path="/" element={<Home logged_in={logged_in} />} />
-            <Route path="/statistics" element={<Statistics id={id} />} />
-            <Route path="/profile" element={<Profile id={id} />} />
-            <Route path="*" element={<Navigate to="/" />} />
-          </Routes>
-        </div>
-      </BrowserRouter>
-    </Container>
+    <>
+      {!isLoggedIn ? (
+        <Welcome setIsLoggedIn={setIsLoggedIn} />
+      ) : (
+        <Container fluid className="App bg-rhythm-200">
+          <BrowserRouter>
+            <Header user_id={id} />
+            <div className="max-w-7xl mx-auto min-h-screen px-3 py-2">
+              <Routes>
+                <Route path="/" element={<Home logged_in={isLoggedIn} />} />
+                <Route path="/statistics" element={<Statistics id={id} />} />
+                <Route path="/profile" element={<Profile id={id} />} />
+                <Route path="*" element={<Navigate to="/" />} />
+              </Routes>
+            </div>
+          </BrowserRouter>
+        </Container>
+      )}
+    </>
   );
 }
--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,6 +1,11 @@
 import WelcomeCarousel from "../components/WelcomeCarousel";
+import { Link } from "react-router-dom";
 
-export default function Welcome() {
+export default function Welcome(props) {
+  let logIn = () => {
+    props.setIsLoggedIn(true);
+  };
+
   return (
     // Root div
     <div className="bg-raisin-700 min-h-screen grid place-items-center bg-cover bg-center">
@@ -13,7 +18,11 @@ export default function Welcome() {
       {/* Carousel containing information about the site */}
       <WelcomeCarousel />
       {/* Button to continue */}
-      <button className=" bg-gradient-to-br from-tangerine-50 to-raisin-50 hover:scale-110 hover:drop-shadow-xl hover:bg-raisin-400 drop-shadow-md duration-200 rounded-md mb-15">
+      <button
+        className=" bg-gradient-to-br from-tangerine-50 to-raisin-50 hover:scale-110 hover:drop-shadow-xl hover:bg-raisin-400 drop-shadow-md duration-200 rounded-md mb-15"
+        onClick={logIn}
+      >
+        {/* TODO we will need to change the functionality of this button after backend implementation. */}
         <div className="m-2 text-3xl hover:anime-gradient hover:font-bold font-semibold">
           Show Me More
         </div>
