c00faae7672368f6192a8e2e68370040455ef55c
Fiddle01 <58796478+Fiddle01@users.noreply.github.com>
2023-01-17 13:34:18 -0500

    Update HappinessCommentModal.js

--- src/components/HappinessCommentModal.js
+++ src/components/HappinessCommentModal.js
@@ -1,8 +1,8 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Modal from "react-bootstrap/Modal";
 import LSUForm from "./LSUForm";
 import Journal from "../media/journal-icon.svg";
-import JournalQuestion from "../media/journal-question-icon.svg"
+import JournalQuestion from "../media/journal-question-icon.svg";
 
 export default function HappinessCommentModal(props) {
   const [show, setShow] = useState(false);
@@ -11,10 +11,21 @@ export default function HappinessCommentModal(props) {
 
   return (
     <>
-      <button type="button"
-              onClick={handleShow}
-              className="text-white bg-raisin-300 hover:bg-raisin-400 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-2 mt-3">
-        <img src={props.comment ? Journal : JournalQuestion /* TODO probably doesn't actually work */} aria-hidden="true" className="w-5 h-5 mr-2 -ml-1" viewBox="0 0 20 20"/>
+      <button
+        type="button"
+        onClick={handleShow}
+        className="text-white bg-raisin-300 hover:bg-raisin-400 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-2 mt-3"
+      >
+        <img
+          src={
+            props.comment
+              ? Journal
+              : JournalQuestion /* TODO probably doesn't actually work */
+          }
+          aria-hidden="true"
+          className="w-5 h-5 mr-2 -ml-1"
+          viewBox="0 0 20 20"
+        />
         Add journal entry
       </button>
 
@@ -32,14 +43,20 @@ export default function HappinessCommentModal(props) {
 
         <Modal.Body>
           <div className="mb-6">
-            <label htmlFor="large-input" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Large
-              input</label>
-            <textarea  id="large-input" value={props.comment}
-                   className="block w-full p-4 bg-gray-100 rounded focus:border-blue-500 outline-none focus:border-raisin-200 border-2 focus:border-4"
-                  onChange={(e) => {
-                    props.setComment(e.target.value)
-                  }
-                  }/>
+            <label
+              htmlFor="large-input"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Large input
+            </label>
+            <textarea
+              id="large-input"
+              value={props.comment}
+              className="block w-full p-4 bg-gray-100 rounded focus:border-blue-500 outline-none focus:border-raisin-200 border-2 focus:border-4"
+              onChange={(e) => {
+                props.setComment(e.target.value);
+              }}
+            />
           </div>
         </Modal.Body>
         <Modal.Footer>
