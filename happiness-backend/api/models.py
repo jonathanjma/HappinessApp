919cbca8bf3f8d26c3d4c7dcb09a60c356bc3684
Fiddle01 <58796478+zachseidner1@users.noreply.github.com>
2023-01-21 17:35:50 -0500

    Added User and Setting models.

--- happiness-backend/api/models.py
+++ happiness-backend/api/models.py
@@ -1,1 +1,117 @@
-# put sqlalchemy database models here
+import datetime
+import hashlib
+import os
+import bcrypt
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class User(db.Model):
+    """
+    User model. Has a one-to-many relationship with setting table.
+    """
+    __tablename__ = "user"
+    id = db.Column(db.Integer, primary_key=True, auto_increment=True)
+
+    # User information
+    email = db.Column(db.String, nullable=False, unique=True)
+    username = db.Column(db.String, nullable=False, unique=True)
+    password_digest = db.Column(db.String, nullable=False)
+    profile_picture = db.Column(db.String, nullable=False)  # Will represent an AWS URL
+    settings = db.relationship("Setting", cascade="delete")
+
+    # Session information
+    session_token = db.Column(db.String, nullable=False, unique=True)
+    session_expiration = db.Column(db.DateTime, nullable=False)
+    update_token = db.Column(db.String, nullable=False, unique=True)
+
+    def __init__(self, **kwargs):
+        """
+        Initializes a User object
+        """
+        self.email = kwargs.get("email")
+        # Convert raw password into encrypted string that can still be decrypted, but we cannot decrypt it.
+        self.password_digest = bcrypt.hashpw(kwargs.get("password").encode("utf8"), bcrypt.gensalt(rounds=13))
+        self.renew_session()
+
+    def serialize(self):
+        """
+        Serializes user object into readable JSON.
+        Omits email password, and session information for security reasons
+        """
+        return {
+            "id": self.id,
+            "username": self.username,
+            "profile picture": self.profile_picture,
+            "settings": [setting.serialize() for setting in self.settings]
+        }
+
+    def _urlsafe_base_64(self):
+        """
+        Randomly generates hashed tokens (used for session/update tokens)
+        """
+        return hashlib.sha1(os.urandom(64)).hexdigest()
+
+    def renew_session(self):
+        """
+        Renews the sessions, i.e.
+        1. Creates a new session token
+        2. Sets the expiration time of the session to be a week from now
+        3. Creates a new update token
+        """
+        self.session_token = self._urlsafe_base_64()
+        self.session_expiration = datetime.datetime.now() + datetime.timedelta(weeks=1)
+        self.update_token = self._urlsafe_base_64()
+
+    def verify_password(self, password):
+        """
+        Verifies the password of a user
+        """
+        return bcrypt.checkpw(password.encode("utf8"), self.password_digest)
+
+    def verify_session_token(self, session_token):
+        """
+        Verifies the session token of a user
+        """
+        return session_token == self.session_token and datetime.datetime.now() < self.session_expiration
+
+    def verify_update_token(self, update_token):
+        """
+        Verifies the update token of a user
+        """
+        return update_token == self.update_token
+
+
+class Setting(db.Model):
+    """
+    Settings model. Has a many-to-one relationship with User.
+    To store settings I chose to use the property bag method [1]
+    [1]: https://shorturl.at/uDF07
+    """
+    __tablename__ = "setting"
+    id = db.Column(db.Integer, primary_key=True, auto_increment=True)
+    key = db.Column(db.String, nullable=False)
+    value = db.Column(db.Boolean, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    user = db.relationship("Course", back_populates="settings")
+
+    def __init__(self, **kwargs):
+        """
+        Initializes a setting.
+        Requires that kwargs contains key, value, user_id
+        """
+        self.key = kwargs.get("key")
+        self.value = kwargs.get("value")
+        self.user_id = kwargs.get("user_id")
+
+    def serialize(self):
+        """
+        Serializes a user settings object for returning JSON
+        """
+        return {
+            "id": self.id,
+            "key": self.key,
+            "value": self.value,
+            "user_id": self.user_id
+        }
