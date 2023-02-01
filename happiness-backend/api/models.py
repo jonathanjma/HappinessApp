7424cac2d5ee7a3d38a6ef02cbe169ee5f0eeeaa
Jonathan Ma <jonathanjma03@gmail.com>
2023-02-01 18:28:17 -0500

    refactored token auth code to improve usability

--- happiness-backend/api/models.py
+++ happiness-backend/api/models.py
@@ -1,6 +1,6 @@
-import datetime
 import hashlib
 import os
+from datetime import datetime, timedelta
 
 import bcrypt
 
@@ -37,7 +37,7 @@ class User(db.Model):
         self.password_digest = bcrypt.hashpw(kwargs.get("password").encode("utf8"), bcrypt.gensalt(rounds=13))
         self.username = kwargs.get("username")
         self.profile_picture = kwargs.get("profile_picture", "default")
-        self.renew_session()
+        self.get_token()
 
     def serialize(self):
         """
@@ -51,41 +51,38 @@ class User(db.Model):
             "settings": [setting.serialize() for setting in self.settings]
         }
 
-    def _urlsafe_base_64(self):
-        """
-        Randomly generates hashed tokens (used for session/update tokens)
-        """
-        return hashlib.sha1(os.urandom(64)).hexdigest()
-
-    def renew_session(self):
-        """
-        Renews the sessions, i.e.
-        1. Creates a new session token
-        2. Sets the expiration time of the session to be a week from now
-        3. Creates a new update token
-        """
-        self.session_token = self._urlsafe_base_64()
-        self.session_expiration = datetime.datetime.now() + datetime.timedelta(weeks=1)
-        self.update_token = self._urlsafe_base_64()
-
     def verify_password(self, password):
         """
         Verifies the password of a user
         """
         return bcrypt.checkpw(password.encode("utf8"), self.password_digest)
 
-    def verify_session_token(self, session_token):
+    def _urlsafe_base_64(self):
+        """
+        Randomly generates hashed tokens (used for session/update tokens)
+        """
+        return hashlib.sha1(os.urandom(64)).hexdigest()
+
+    def get_token(self):
+        """
+        Generates a new session token for a user
+        """
+        self.session_token = self._urlsafe_base_64()
+        self.session_expiration = datetime.utcnow() + timedelta(weeks=1)
+        self.update_token = self._urlsafe_base_64()  # don't need anymore?
+        return self.session_token
+
+    def revoke_token(self):
+        """
+        Expires the session token of a user
+        """
+        self.session_expiration = datetime.utcnow() - timedelta(seconds=1)
+
+    def verify_session_token(self):
         """
         Verifies the session token of a user
         """
-        return session_token == self.session_token and datetime.datetime.now() < self.session_expiration
-
-    def verify_update_token(self, update_token):
-        """
-        Verifies the update token of a user
-        """
-        return update_token == self.update_token
-
+        return self.session_expiration > datetime.utcnow()
 
 class Setting(db.Model):
     """
--- happiness-backend/api/user.py
+++ happiness-backend/api/user.py
@@ -1,30 +1,13 @@
-import datetime
-
 from flask import Blueprint
+from flask import json, request
+
+from api import users_dao
+from api.app import db
 from api.models import User, Setting
 from api.responses import success_response, failure_response
-from flask import json, request
-from api.app import db
-from api import users_dao
+from api.token import token_auth
 
 user = Blueprint('user', __name__)
-
-
-def extract_token(my_request):
-    """
-    Helper function that extracts the token from the header of a request
-    """
-    auth_header = my_request.headers.get("Authorization")
-    if auth_header is None:
-        return False, failure_response("Missing authorization header", 400)
-
-    # Header looks like "Authorization: Bearer <token>"
-    bearer_token = auth_header.replace("Bearer ", "").strip()
-    if bearer_token is None or not bearer_token:
-        return False, failure_response("Invalid authorization header", 400)
-
-    return True, bearer_token
-
 
 @user.post('/')
 def create_user():
@@ -43,55 +26,11 @@ def create_user():
     db.session.add(current_user)
     db.session.commit()
 
-    return success_response({
-        "session_token": current_user.session_token,
-        "session_expiration": str(current_user.session_expiration),
-        "update_token": current_user.update_token,
-    }, 201)
-
-
-@user.post('/settings/')
-def add_user_setting():
-    """
-    Adds a setting to the current user's property bag.
-    :return: A JSON success response that contains the added setting, or a failure response.
-    """
-    body = json.loads(request.data)
-    success, token = extract_token(request)
-    if not success:
-        return failure_response("Session token not found. Relog?")
-    current_user = users_dao.get_user_by_session_token(token)
-    if current_user is None or not current_user.verify_session_token(token):
-        return failure_response("Current user not found. Relog?")
-    key, value = body.get("key"), body.get("value")
-    if key is None or value is None:
-        return failure_response("Insufficient setting information provided", 400)
-    setting = Setting(key=key, value=value, user_id=current_user.id)
-    db.session.add(setting)
-    db.session.commit()
-
-    return success_response(setting.serialize(), 201)
-
-
-@user.get('/settings/')
-def get_user_settings():
-    """
-    Gets the settings of the current user by authorization token.
-    :return: A JSON response of a list of key value pairs that contain setting keys and their values for the user.
-    """
-    success, token = extract_token(request)
-    if not success:
-        return failure_response("Session token not found. Relog?")
-    current_user = users_dao.get_user_by_session_token(token)
-    if current_user is None or not current_user.verify_session_token(token):
-        return failure_response("User with current session token not found. Relog?")
-    settings = Setting.query.filter(Setting.user_id == current_user.id).all()
-    return success_response({
-        "settings": [(s.key, s.value) for s in settings]
-    })
+    return success_response({"session_token": current_user.session_token}, 201)
 
 
 @user.get('/')
+@token_auth.login_required
 def get_user_by_id():
     """
     This method gets user information from a user by querying the user by id.
@@ -99,12 +38,6 @@ def get_user_by_id():
     TODO this method should only be allowed to be called by someone in the same group as the target user.
     :return: JSON of User object containing user information
     """
-    success, token = extract_token(request)
-    if not success or token is None:
-        return failure_response("You need to be logged in to perform this action", 401)
-    current_user = users_dao.get_user_by_session_token(token)
-    if current_user is None or not current_user.verify_session_token(token):
-        return failure_response("You need to be logged in to perform this action", 401)
 
     body = json.loads(request.data)
     friend_id = body.get("id")
@@ -118,67 +51,47 @@ def get_user_by_id():
         "settings": [s.serialize() for s in friend_user.settings],
     })
 
-
-@user.post('/logout/')
-def logout_user():
-    """
-    Logs out the current user.
-    :return: a success or failure response based on whether we were able to log out the user.
-    """
-    success, token = extract_token(request)
-    if not success or token is None:
-        return failure_response("User not found")
-    current_user = users_dao.get_user_by_session_token(token)
-    if current_user is None:
-        return failure_response("User not found")
-
-    current_user.session_token = ""
-    current_user.session_expiration = datetime.datetime.now()
-    current_user.update_token = ""
-    db.session.commit()
-
-    return success_response({"message": "You have successfully logged out"})
-
-
-@user.post('/login/')
-def login_user():
-    body = json.loads(request.data)
-    email, password = body.get("email"), body.get("password")
-    if email is None or password is None:
-        return failure_response("Provide all login credentials", 400)
-    success, current_user = users_dao.verify_credentials(email, password)
-
-    if not success:
-        return failure_response("Incorrect login credentials", 401)
-
-    if current_user is None:
-        return failure_response("User not found", 404)
-
-    if datetime.datetime.now() > current_user.session_expiration:
-        print("hello")
-        current_user.renew_session()
-        db.session.commit()
-
-    return success_response({
-        "session_token": current_user.session_token,
-        "session_expiration": str(current_user.session_expiration),
-        "update_token": current_user.update_token,
-    })
-
-
 @user.delete('/')
 def delete_user():
     """
     Deletes the user that is currently logged in, including all user data.
     :return: A success with serialized user or failure response with the appropriate message.
     """
-    success, token = extract_token(request)
-    if not success:
-        return failure_response("You must be logged in to perform this action", 401)
-    current_user = users_dao.get_user_by_session_token(token)
-    if current_user is None or not current_user.verify_session_token(token):
-        return failure_response("Your account was not found.", 404)
-
+    current_user = token_auth.current_user()
     db.session.delete(current_user)
     db.session.commit()
+
     return success_response(current_user.serialize(), 200)
+
+
+@user.post('/settings')
+@token_auth.login_required
+def add_user_setting():
+    """
+    Adds a setting to the current user's property bag.
+    :return: A JSON success response that contains the added setting, or a failure response.
+    """
+    body = json.loads(request.data)
+    current_user = token_auth.current_user()
+    key, value = body.get("key"), body.get("value")
+    if key is None or value is None:
+        return failure_response("Insufficient setting information provided", 400)
+    setting = Setting(key=key, value=value, user_id=current_user.id)
+    db.session.add(setting)
+    db.session.commit()
+
+    return success_response(setting.serialize(), 201)
+
+
+@user.get('/settings')
+@token_auth.login_required
+def get_user_settings():
+    """
+    Gets the settings of the current user by authorization token.
+    :return: A JSON response of a list of key value pairs that contain setting keys and their values for the user.
+    """
+    current_user = token_auth.current_user()
+    settings = Setting.query.filter(Setting.user_id == current_user.id).all()
+    return success_response({
+        "settings": [(s.key, s.value) for s in settings]
+    })
