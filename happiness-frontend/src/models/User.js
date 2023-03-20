0f44ac08d85de70644ab1ab265db043ed92f7121
zachseidner1 <zachary.seidner@gmail.com>
2023-03-20 14:52:34 -0400

    Implemented Untested UserProvider - Completed UserProvider.js (although it is untested) - Next steps are to use ApiClient and try to test the file.

---
+++ happiness-frontend/src/models/User.js
@@ -0,0 +1,10 @@
+class User {
+  constructor(id, email, profile_picture, password, settings, username) {
+    this.id = id;
+    this.email = email;
+    this.profile_picture = profile_picture;
+    this.password = password;
+    this.settings = settings;
+    this.username = username;
+  }
+}
