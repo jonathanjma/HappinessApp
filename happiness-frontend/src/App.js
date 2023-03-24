2311e3d8392332999388510c2b7ad096f7912d6b
Jonathan Ma <jonathanjma03@gmail.com>
2023-03-24 14:59:24 -0400

    Added private and public routes components
    - realized there is a bug with User provider

--- happiness-frontend/src/App.js
+++ happiness-frontend/src/App.js
@@ -5,46 +5,67 @@ import Header from "./components/Header";
 import Welcome from "./pages/Welcome";
 import Container from "react-bootstrap/Container";
 import Settings from "./pages/Settings";
-import Home from "./pages/Home";
-import { useState } from "react";
 import History from "./pages/History";
 import UserGroups from "./pages/UserGroups";
 import Group from "./pages/Group";
 import "./App.css";
 import ApiProvider from "./contexts/ApiProvider";
+import UserProvider from "./contexts/UserProvider";
+import PrivateRoute from "./components/PrivateRoute";
+import PublicRoute from "./components/PublicRoute";
+import SubmitHappiness from "./pages/SubmitHappiness";
 
 // change id number to id of user in Users.js (temporary until backend + login set up)
 export default function App() {
   const id = 1;
-  //TODO this is a placeholder with basic functionality, we will have to rewrite after backend implementation.
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
 
   return (
-    <>
-      {!isLoggedIn ? (
-        // <Welcome setIsLoggedIn={setIsLoggedIn} />
-        <Welcome />
-      ) : (
-        <Container fluid className="App bg-buff-50">
-          <BrowserRouter>
-            <ApiProvider>
-              <Header user_id={id} />
-              <div className="max-w-7xl mx-auto min-h-screen px-3 py-2">
-                <Routes>
-                  <Route path="/" element={<Home logged_in={isLoggedIn} />} />
-                  <Route path="/statistics" element={<Statistics id={id} />} />
-                  <Route path="/profile" element={<Profile id={id} />} />
-                  <Route path="/groups" element={<UserGroups id={id} />} />
-                  <Route path="/groups/:groupID" element={<Group id={id} />} />
-                  <Route path="/settings" element={<Settings />} />
-                  <Route path="/history" element={<History id={id} />} />
-                  <Route path="*" element={<Navigate to="/" />} />
-                </Routes>
-              </div>
-            </ApiProvider>
-          </BrowserRouter>
-        </Container>
-      )}
-    </>
+    <Container fluid className="App bg-buff-50">
+      <BrowserRouter>
+        <ApiProvider>
+          <UserProvider>
+            <Header user_id={id} />
+            <div className="max-w-7xl mx-auto min-h-screen px-3 py-2">
+              <Routes>
+                <Route
+                  path="/"
+                  element={
+                    <PublicRoute>
+                      <Welcome />
+                    </PublicRoute>
+                  }
+                />
+                <Route
+                  path="*"
+                  element={
+                    <PrivateRoute>
+                      <Routes>
+                        <Route path="/home" element={<SubmitHappiness />} />
+                        <Route
+                          path="/statistics"
+                          element={<Statistics id={id} />}
+                        />
+                        <Route path="/profile" element={<Profile id={id} />} />
+                        <Route
+                          path="/groups"
+                          element={<UserGroups id={id} />}
+                        />
+                        <Route
+                          path="/groups/:groupID"
+                          element={<Group id={id} />}
+                        />
+                        <Route path="/settings" element={<Settings />} />
+                        <Route path="/history" element={<History id={id} />} />
+                        <Route path="*" element={<Navigate to="/" />} />
+                      </Routes>
+                    </PrivateRoute>
+                  }
+                />
+              </Routes>
+            </div>
+          </UserProvider>
+        </ApiProvider>
+      </BrowserRouter>
+    </Container>
   );
 }
---
+++ happiness-frontend/src/components/PrivateRoute.js
@@ -0,0 +1,19 @@
+import { Navigate, useLocation } from "react-router-dom";
+import { useUser } from "../contexts/UserProvider";
+import { Keys } from "../keys";
+
+export default function PrivateRoute({ children }) {
+  const { user: userState } = useUser();
+  const location = useLocation();
+
+  console.log(userState);
+
+  if (userState.type === Keys.LOADING) {
+    return null;
+  } else if (userState.type === Keys.SUCCESS) {
+    return children;
+  } else {
+    const url = location.pathname + location.search + location.hash;
+    return <Navigate to="/welcome" state={{ next: url }} />;
+  }
+}
