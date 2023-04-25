6deeb04944b2c83cd85a0d30c08e02b40b8683c7
Jonathan Ma <jonathanjma03@gmail.com>
2023-04-25 00:48:03 -0400

    remove old url

--- happiness-backend/config.py
+++ happiness-backend/config.py
@@ -17,7 +17,6 @@ class Config:
     # Security measures
     SECURITY_PASSWORD_SALT = os.environ.get("SECURITY_PASSWORD_SALT")
     SECRET_KEY = os.environ.get("SECRET_KEY")
-    SERVER_NAME = "happiness-qfvoiwgqdq-ue.a.run.app"
 
     # Email sending
     MAIL_SERVER = "smtp.gmail.com"
