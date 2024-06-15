c9330c2ae95a7e1ecc155ebffd507cd46827ab6f
Jonathan Ma <jonathanjma03@gmail.com>
2024-06-14 19:42:45 -0500

    remove old frontend

--- happiness-frontend/src/resources/AccountSettings.js
+++
@@ -1,7 +0,0 @@
-import Exit from "../media/exit-icon.svg";
-import DeleteUser from "../media/delete-user-icon.svg";
-
-export const accountSettings = [
-  { id: 7, name: "Sign Out", icon: Exit, isSensitive: true },
-  { id: 8, name: "Delete Account", icon: DeleteUser, isSensitive: true },
-];
